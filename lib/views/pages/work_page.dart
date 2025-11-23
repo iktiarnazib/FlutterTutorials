df814da1eb10389ef49f400c76770331d9053453
iktiar <iktiar@iktiars-MacBook-Air.local>
2025-11-23 20:15:41 +0600

    Switch

--- lib/views/pages/work_page.dart
+++ lib/views/pages/work_page.dart
@@ -11,6 +11,7 @@ class _WorkPageState extends State<WorkPage> {
   TextEditingController controller = TextEditingController();
   bool? isChecked = true;
   bool? isChecked1 = true;
+  bool isOn = true;
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -70,6 +71,34 @@ class _WorkPageState extends State<WorkPage> {
           onChanged: (bool? value) {
             setState(() {
               isChecked = value;
+            });
+          },
+        ),
+        Switch(
+          value: isOn,
+          onChanged: (bool value) {
+            setState(() {
+              isOn = value;
+            });
+          },
+        ),
+        SwitchListTile(
+          title: Row(
+            children: [
+              Icon(Icons.turned_in, color: Colors.amber),
+              Text("Saved", style: TextStyle(fontSize: 20.0)),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [Text("Switch"), Icon(Icons.lock_clock)],
+                ),
+              ),
+            ],
+          ),
+          value: isOn,
+          onChanged: (bool value) {
+            setState(() {
+              isOn = value;
             });
           },
         ),
