9e1b8fe05375d9e664529fb77e0c3d6ed6e210a9
iktiar <iktiar@iktiars-MacBook-Air.local>
2025-11-23 14:14:45 +0600

    Text field example with decoration: inputdecoration, border: outlineinputborder

--- lib/views/pages/work_page.dart
+++ lib/views/pages/work_page.dart
@@ -1,17 +1,31 @@
 import 'package:flutter/material.dart';
 
-class WorkPage extends StatelessWidget {
+class WorkPage extends StatefulWidget {
   const WorkPage({super.key});
 
   @override
+  State<WorkPage> createState() => _WorkPageState();
+}
+
+class _WorkPageState extends State<WorkPage> {
+  TextEditingController controller = TextEditingController();
+  @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        height: 100.0,
-        width: 100.0,
-        color: Colors.teal,
-        child: Text("This is work page"),
-      ),
+    return Column(
+      children: [
+        Text("Plesae input your text!"),
+        Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: TextField(
+            controller: controller,
+            decoration: InputDecoration(border: OutlineInputBorder()),
+            onEditingComplete: () {
+              setState(() {});
+            },
+          ),
+        ),
+        Text(controller.text),
+      ],
     );
   }
 }
