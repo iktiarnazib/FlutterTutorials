aac75538bc9324de2664e66aa8ce469b6e704048
Iktiar Nazib <iktiarnazib@gmail.com>
2025-11-26 18:12:07 +0600

    Log out with Hero widget optimized

--- linux/lib/views/pages/profile_page.dart
+++ linux/lib/views/pages/profile_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-import '../../data/notifiers.dart';
 import 'home_page.dart';
 import 'settings_page.dart';
 import 'welcome_page.dart';
@@ -14,24 +13,23 @@ class ProfilePage extends StatelessWidget {
       child: Column(
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
-          ValueListenableBuilder(
-            valueListenable: selectedIndexNotifier,
-            builder:
-                (BuildContext context, dynamic IndexNotifier, Widget? child) {
-                  return ListTile(
-                    onTap: () {
-                      Navigator.pushReplacement(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) {
-                            return WelcomePage();
-                          },
-                        ),
-                      );
+          SizedBox(
+            height: 60.0,
+            child: ListTile(
+              tileColor: Colors.pink,
+              onTap: () {
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) {
+                      return WelcomePage();
                     },
-                    title: Text("Log Out", style: TextStyle(fontSize: 20.0)),
-                  );
-                },
+                  ),
+                );
+              },
+
+              title: Text("Log Out", style: TextStyle(fontSize: 20.0)),
+            ),
           ),
         ],
       ),
