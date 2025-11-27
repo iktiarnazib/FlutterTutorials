ea0aeb3c6a881d172ebf122bfae60605d08f49ef
Iktiar Nazib <iktiarnazib@gmail.com>
2025-11-27 14:52:20 +0600

    Divider and Vertical Divider

--- lib/views/pages/home_page.dart
+++ lib/views/pages/home_page.dart
@@ -12,16 +12,17 @@ class HomePage extends StatelessWidget {
       child: Column(
         children: [
           HeroWidget(),
+
           SizedBox(height: 20.0),
           OutlinedButton(
             onPressed: () {
               ScaffoldMessenger.of(context).showSnackBar(
                 SnackBar(
-                  duration: Duration(seconds: 5),
-                  backgroundColor: Colors.teal,
                   behavior: SnackBarBehavior.floating,
+                  backgroundColor: Colors.pink,
+                  duration: Duration(seconds: 5),
                   content: Text(
-                    "The Snackbar has been pressed",
+                    "This is the snackbar",
                     style: TextStyle(color: Colors.white),
                   ),
                 ),
@@ -29,33 +30,35 @@ class HomePage extends StatelessWidget {
             },
             child: Text("Show Snackbar"),
           ),
+          Divider(
+            height: 20.0,
+            thickness: 2.0,
+            color: Colors.teal,
+            indent: 30.0,
+            endIndent: 30.0,
+          ),
+          Container(height: 30.0, child: VerticalDivider(color: Colors.blue)),
           OutlinedButton(
             onPressed: () {
               showDialog(
                 context: context,
                 builder: (context) {
                   return AlertDialog.adaptive(
-                    title: Text(
-                      "This is alert Dialog",
-                      style: TextStyle(color: Colors.red),
-                    ),
-                    content: Text("You shouldn't press the alert dialog!"),
+                    title: Text("Alert Dialog"),
+                    content: Text("Text for Alert Dialog"),
                     actions: [
-                      GestureDetector(
-                        onTap: () {
+                      TextButton(
+                        onPressed: () {
                           Navigator.pop(context);
                         },
-                        child: Container(
-                          height: 40.0,
-                          child: Center(child: Text("Close")),
-                        ),
+                        child: Text('close'),
                       ),
                     ],
                   );
                 },
               );
             },
-            child: Text("Alert Dialog"),
+            child: Text('Alert Dialog'),
           ),
         ],
       ),
