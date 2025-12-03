5c6d6a63281a63e48ec574cf8c3d7f92f0cde458
Iktiar Nazib <iktiarnazib@gmail.com>
2025-12-03 18:43:04 +0600

    Make the app scrollable

--- lib/views/pages/welcome_page.dart
+++ lib/views/pages/welcome_page.dart
@@ -12,58 +12,64 @@ class WelcomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Lottie.asset("assets/lotties/Home.json"),
-            FittedBox(
-              child: Text(
-                "Welcome to Flutter",
-                style: TextStyle(
-                  fontSize: 150.0,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 10.0,
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Lottie.asset("assets/lotties/Home.json"),
+                FittedBox(
+                  child: Text(
+                    "Welcome to Flutter",
+                    style: TextStyle(
+                      fontSize: 150.0,
+                      fontWeight: FontWeight.bold,
+                      letterSpacing: 10.0,
+                    ),
+                  ),
                 ),
-              ),
-            ),
-            SizedBox(height: 20.0),
-            FilledButton(
-              onPressed: () {
-                selectedIndexNotifier.value = 0;
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return RegisterPage();
-                    },
+                SizedBox(height: 20.0),
+                FilledButton(
+                  onPressed: () {
+                    selectedIndexNotifier.value = 0;
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return RegisterPage();
+                        },
+                      ),
+                    );
+                  },
+                  style: FilledButton.styleFrom(
+                    minimumSize: Size(double.infinity, 40.0),
                   ),
-                );
-              },
-              style: FilledButton.styleFrom(
-                minimumSize: Size(double.infinity, 40.0),
-              ),
-              child: Text("Register"),
-            ),
-            OutlinedButton(
-              onPressed: () {
-                selectedIndexNotifier.value = 0;
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return LoginPage();
-                    },
+                  child: Text("Register"),
+                ),
+                OutlinedButton(
+                  onPressed: () {
+                    selectedIndexNotifier.value = 0;
+                    Navigator.pushReplacement(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return LoginPage();
+                        },
+                      ),
+                    );
+                  },
+                  style: OutlinedButton.styleFrom(
+                    minimumSize: Size(double.infinity, 40.0),
                   ),
-                );
-              },
-              style: OutlinedButton.styleFrom(
-                minimumSize: Size(double.infinity, 40.0),
-              ),
-              child: Text("Login"),
+                  child: Text("Login"),
+                ),
+                SizedBox(height: 20.0),
+              ],
             ),
-          ],
+          ),
         ),
       ),
     );
